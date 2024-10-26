91a930d55bafb648bcf4898b2039c86db31e0d62
westernscreenowl <loguelei000@frogrock.org>
2024-10-25 21:35:20 -0700

    Update main.c

--- main.c
+++ main.c
@@ -26,37 +26,37 @@ if (selfht = true) {
     int hitarea = rand() % 11; 
     switch (hitarea) {
         case 0:
-        printf("The Attack Missed!")
+        printf("The Attack Missed!");
 
         case 1:
-        printf("The Attack Missed!")
+        printf("The Attack Missed!");
         
         case 2:
-        printf("The Attack Missed!")
+        printf("The Attack Missed!");
         
         case 3:
-        printf("The Attack Hit Your Feet!")
+        printf("The Attack Hit Your Feet!");
         
         case 4:
-        printf("The Attack Hit Your Right Leg!")
+        printf("The Attack Hit Your Right Leg!");
         
         case 5:
-        printf("The Attack Hit Your Torso!")
+        printf("The Attack Hit Your Torso!");
         
         case 6:
-        printf("The Attack Hit Your Right Hand!")
+        printf("The Attack Hit Your Right Hand!");
         
         case 7:
-        printf("The Attack Hit Your Right Arm!")
+        printf("The Attack Hit Your Right Arm!");
         
         case 8:
-        printf("The Attack Hit Your Left Hand!")
+        printf("The Attack Hit Your Left Hand!");
         
         case 9:
-        printf("The Attack Hit Your Left Arm!")
+        printf("The Attack Hit Your Left Arm!");
         
         case 10:
-        printf("The Attack Hit Your Head!")
+        printf("The Attack Hit Your Head!");
     }
     }
 }
