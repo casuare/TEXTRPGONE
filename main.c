1b39fad234b46d018c11feb4dd012d59eddff5ad
casuare <coledeussen@gmail.com>
2024-10-25 19:02:06 -0700

    adjustment

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include <stdbool.h>
 
 int combat() {
-int enemwnd[] = {0,0,0,0,0,0,0,0,0};
+int enemwnd[] = {0,0,0,0,0,0,0};
 }
 int main() {
-int selfwnd[] = {0,0,0,0,0,0,0,0,0};
+int selfwnd[] = {0,0,0,0,0,0,0};
 int snty = 10;
 srand(time(NULL));  
 int hitarea = 0;
